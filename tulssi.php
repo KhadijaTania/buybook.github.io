fda75eda60b937b825981f1f8f77db09b7b3531a
Khadija Sifat <khadijasifat096@gmail.com>
2019-01-02 11:13:07 +0600

    first github upload

---
+++ tulssi.php
@@ -0,0 +1,69 @@
+
+<html>
+<head>
+<br>
+<style>
+pre{
+	color:red;
+  
+}
+.h img {
+  
+  width: 80px;
+  height: 80px;
+  background: lightgreen;
+}
+body{
+	
+	background:lightgreen;
+}
+h2{
+	color:red;
+	
+}
+
+h3{
+	color:black;
+	
+}
+<br>
+<br>
+
+
+</style>
+</head>
+<div class="h">
+
+</div>
+<h2 style="font-size:40px;" align="left">About Tulshi</h2>
+<body align="left">
+
+<p style="font-size:25px;">Tulsi also known as basil leaves, is a fairly common plant in Indian households.
+ Considered holy by many
+religions, the tulsi plant is revered for its divine properties.
+Besides praying to the plant, a number of people advice including the leaves and roots of the plant in various
+medical decoctions.
+</p>
+<h2 style="font-size:40px;" align="left">Here
+are the top 10 benefits of tulsi.</h2>
+
+ </body>
+ <p align="left" style="font-size:25px;">
+               <h3> Cures a fever:</h3> Tulsi has very potent germicidal, fungicidal, anti-bacterial and anti-biotic properties that are great
+for resolving fevers. It has the potential to cure any fever right from those caused due to common infections to
+those caused due to malaria as well. In ayurveda, it is strongly advised that a person suffering from fever should
+have a decoction made of tulsi leaves. In case of a fever boil a few leaves of tulsi with powdered cardamom in hal
+a litre of water( The proportion of tulsi to cardamom powder should be in the ratio 1:0.3). Let it reduce to half its
+total volume. Mix this decoction with sugar and milk. Sip every two to three hours. This remedy is especially
+good for children.</p>
+<p><h3>Beats diabetes:</h3> leaves of holy basil are packed with antioxidants and essential oils that produce eugenol, methyl
+eugenol and caryophyllene. Collectively these substances help the pancreatic beta cells (cells that store and
+release insulin) function properly. This in turn helps increase sensitivity to insulin. Lowering one’s blood sugar
+and treating diabetes effectively. An added advantage is that the antioxidants present in the leaves help beat the
+ill effects of oxidative stress.</p>
+<p><h3>Protects the heart: </h3>Tulsi has a powerful anti-oxidant component called Eugenol. This compound helps protect the
+heart by keeping one’s blood pressure under control and lowering his/her cholesterol levels. Chewing a few
+leaves of tulsi on an empty stomach everyday can both prevent and protect any heart ailments.</p>
+
+</p>
+</html>
